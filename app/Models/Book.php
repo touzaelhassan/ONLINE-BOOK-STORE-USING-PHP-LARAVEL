432227b689820d0e4da51f80782da4b2df35d265
touza <touzaelhassan@gmail.com>
2022-05-08 19:32:10 +0100

    Creating model methods to interact with The database

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo('App\Publisher');
+    }
+
+    public function authors()
+    {
+        return $this->belongsToMany('App\Author', 'book_author');
+    }
 }
